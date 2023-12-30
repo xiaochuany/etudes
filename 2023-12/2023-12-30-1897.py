98dd0072ed68c6e0b3f28690e08a7bf28e645f55
xiaochuany <xiaochuan.j.yang@gmail.com>
2023-12-30 10:43:14 +0100

    chain

---
+++ 2023-12/2023-12-30-1897.py
@@ -0,0 +1,13 @@
+#2023-12-30-1897
+from itertools import chain
+from collections import Counter
+from typing import List
+
+def make_equal(words:List[str])->bool:
+    return all(v%len(words)==0 for v in Counter(chain(*words)).values())
+
+class TestMakeEqual:
+    def test(self):
+        assert make_equal(['abb','bba','bab']) is True
+        assert make_equal(['ab','a']) is False
+        assert make_equal(['aabb','aa']) is True
