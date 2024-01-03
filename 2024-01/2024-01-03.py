c513f4455cd5a4dfd64b72c09bcf8b760d29f180
xiaochuany <xiaochuan.j.yang@gmail.com>
2024-01-03 10:30:03 +0100

    mystr.count

---
+++ 2024-01/2024-01-03.py
@@ -0,0 +1,14 @@
+# %%
+from typing import List
+
+# %%
+def number_beams(bank: List[str]) -> int:
+    rs = []
+    for row in bank:
+        if (a:=row.count('1')): rs.append(a)
+    return sum([pre*nex for pre, nex in zip(rs,rs[1:])])
+
+# %%
+def test_number_beams():
+    assert number_beams(['001','111', '000', '1111'])==15
+    assert number_beams(['000','0111','00'])==0
