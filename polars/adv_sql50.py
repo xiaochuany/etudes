239b0cee3c455a44e8801f41484c79777a111a04
xiaochuany <xiaochuan.j.yang@gmail.com>
2024-04-17 22:41:58 +0200

    1398 join;  str.contains;  str.concat

--- polars/adv_sql50.py
+++ polars/adv_sql50.py
@@ -31,22 +31,48 @@ print(q)
 #1873 The bonus of an employee is 100% of their salary if the ID of the employee is an odd number
 # and the employee's name does not start with the character 'M'. The bonus of an employee is 0 otherwise.
 
-## TODO: when then otherwise clause 
 
 data = [[2, 'Meir', 3000], [3, 'Michael', 3800], [7, 'Addilyn', 7400], [8, 'Juan', 6100], [9, 'Kannon', 7700]]
 employees = pd.DataFrame(data, columns=['employee_id', 'name', 'salary']).astype({'employee_id':'int64', 'name':'object', 'salary':'int64'})
 
 df = pl.from_pandas(employees)
 
-mask = ~df["name"].str.starts_with("M") & (df["employee_id"]%2==1)
+# mask = ~df["name"].str.starts_with("M") & (df["employee_id"]%2==1)
 
+# q = (
+#     df
+#     .select(
+#         pl.col('employee_id'),
+#         pl.col('salary')*mask,
+#     )
+#     .sort(by=["salary","employee_id"], descending=[True,False])
+# )
+
+## using when then otherwise clause 
 q = (
-    df
-    .select(
-        pl.col('employee_id'),
-        pl.col('salary')*mask,
+    df.select(
+        pl.col("employee_id"),
+        pl.when(~pl.col("name").str.starts_with("M") & pl.col("employee_id")%2==1).then(pl.col("salary")).otherwise(0)
     )
-    .sort(by=["salary","employee_id"], descending=[True,False])
+    .sort(by="salary", descending=True)
 )
+
 print(q)
 
+#1398 customers who bought products "A", "B" but did not buy the product "C"
+
+data = [[1, 'Daniel'], [2, 'Diana'], [3, 'Elizabeth'], [4, 'Jhon']]
+customers = pl.from_pandas(pd.DataFrame(data, columns=['customer_id', 'customer_name']).astype({'customer_id':'Int64', 'customer_name':'object'}))
+data = [[10, 1, 'A'], [20, 1, 'B'], [30, 1, 'D'], [40, 1, 'C'], [50, 2, 'A'], [60, 3, 'A'], [70, 3, 'B'], [80, 3, 'D'], [90, 4, 'C']]
+orders = pl.from_pandas(pd.DataFrame(data, columns=['order_id', 'customer_id', 'product_name']).astype({'order_id':'Int64', 'customer_id':'Int64', 'product_name':'object'}))
+
+q = (
+    orders.group_by("customer_id").agg(pl.col("product_name").str.concat(""))
+    .filter(pl.col("product_name").str.contains("A") &
+            pl.col("product_name").str.contains("B") &
+            ~pl.col("product_name").str.contains("C"))
+    .join(customers, on="customer_id")
+    .select(["customer_id","customer_name"])
+)
+
+print(q)
